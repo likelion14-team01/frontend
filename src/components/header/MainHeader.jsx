481652aeeff924867687be086311bdca7e044863
kimseohyeon1533 <kimseohyeon1533@gmail.com>
2026-07-07 11:00:47 +0900

    :recycle: Refactor: MainHeader코드 수정

--- src/components/header/MainHeader.jsx
+++ src/components/header/MainHeader.jsx
@@ -1,36 +1,12 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
-import "./Header.css";
+import "./MainHeader.css";
 
-function Header({ title, showBack = true, rightContent = null }) {
-  const navigate = useNavigate();
-
-  const handleBack = () => {
-    navigate(-1);
-  };
-
+function MainHeader({ title = "홈" }) {
   return (
-    <header className="header">
-      <div className="headerLeft">
-        {showBack && (
-          <button
-            type="button"
-            className="backButton"
-            onClick={handleBack}
-            aria-label="뒤로가기"
-          >
-            ‹
-          </button>
-        )}
-      </div>
-
-      <h1 className="headerTitle">{title}</h1>
-
-      <div className="headerRight">
-        {rightContent}
-      </div>
+    <header className="mainHeader">
+      <h1 className="mainHeaderTitle">{title}</h1>
     </header>
   );
 }
 
-export default Header;
+export default MainHeader;
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,21 @@
-import styled from "styled-components";
+import React from "react";
+import MainHeader from "../../components/header/MainHeader";
+import Header from "../../components/header/Header";
 
-export default function Home() {
-    return (
-        <></>
-    );
+function Home() {
+  return (
+    <div>
+      <MainHeader title="홈" />
+
+      <div style={{ height: "20px", backgroundColor: "#f5f5f5" }} />
+
+      <Header title="식물등록" />
+
+      <main style={{ padding: "20px" }}>
+        <p>헤더 확인용 화면입니다.</p>
+      </main>
+    </div>
+  );
 }
+
+export default Home;
