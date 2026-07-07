152ae796978501d9e160c791bffc30282dffa99d
dbszl3 <yoonji0811@naver.com>
2026-07-07 15:21:22 +0900

    ✨ Feat: 등록하기 페이지 푸터 수정

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -79,7 +79,7 @@ export default function Footer() {
 
     return (
         <FooterContainer>
-            <HomeIcon src={pathname === "/" || pathname.startsWith("/record/") ? homeActive : homeIcon}/>
+            <HomeIcon src={pathname === "/" || pathname.startsWith("/record/") || pathname === "/register" ? homeActive : homeIcon}/>
             <TimelineIcon src={pathname === "/timeline" ? timelineActive : timelineIcon} />
             <MypageIcon src={pathname === "/mypage" ? mypageActive : mypageIcon} />
             <HomeP><MenuSpan>홈</MenuSpan></HomeP>
