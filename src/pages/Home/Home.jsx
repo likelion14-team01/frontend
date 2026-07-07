17076507b5cb7c37498523dbb48febfda92fb6b5
dbszl3 <yoonji0811@naver.com>
2026-07-07 12:39:58 +0900

    ✨ Feat: Home에 TopSection 연결

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import MainHeader from "../../components/header/MainHeader";
+import TopSection from "../TopSection/TopSection.jsx";
 
 function Home() {
   return (
     <div>
       <MainHeader title="홈" />
+      <TopSection />
     </div>
   );
 }
