b0d5910b25d6c0092992d8986f914bf3a94855cb
dbszl3 <yoonji0811@naver.com>
2026-07-07 14:19:30 +0900

    🎨 Design: 홈 및 앱 레이아웃 스타일 수정

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
 import MainHeader from "../../components/header/MainHeader";
 import MyPlantCardList from "../../components/common/myplant/MyPlantCardList";
@@ -9,6 +10,10 @@ import scindapsus from "../../assets/images/scindapsus.svg";
 import lavendar from "../../assets/images/lavendar.svg";
 import monstera from "../../assets/images/monstera.svg";
 
+const HomeContainer = styled.div`
+    height: 1004px;
+`;
+
 function Home() {
   const navigate = useNavigate();
 
@@ -16,6 +21,7 @@ function Home() {
     {
       id: 1,
       name: "몬스테라",
+      path: "/record/monstera",
       image: monstera,
       category: "관엽식물",
       dayText: "22일째",
@@ -25,6 +31,7 @@ function Home() {
     {
       id: 2,
       name: "스킨답서스",
+      path: "/record/scindapsus",
       image: scindapsus,
       category: "관엽식물",
       dayText: "10일째",
@@ -34,6 +41,7 @@ function Home() {
     {
       id: 3,
       name: "라벤더",
+      path: "/record/lavender",
       image: lavendar,
       category: "허브",
       dayText: "5일째",
@@ -43,7 +51,11 @@ function Home() {
   ];
 
   const handleCardClick = (plantId) => {
-    console.log("식물 카드 클릭:", plantId);
+    const selectedPlant = plants.find((plant) => plant.id === plantId);
+
+    if (selectedPlant) {
+      navigate(selectedPlant.path);
+    }
   };
 
   const handleAddClick = () => {
@@ -51,7 +63,7 @@ function Home() {
   };
 
   return (
-    <div>
+    <HomeContainer>
       <MainHeader title="홈" />
       <TopSection />
       <MyPlantCardList
@@ -59,7 +71,7 @@ function Home() {
         onCardClick={handleCardClick}
         onAddClick={handleAddClick}
       />
-    </div>
+    </HomeContainer>
   );
 }
 
