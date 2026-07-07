3a307e393ef5d996b998543c7ff74683e9d29c94
kimseohyeon1533 <kimseohyeon1533@gmail.com>
2026-07-07 11:25:12 +0900

    :sparkles: Refactor: home 화면 수정

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,17 +1,10 @@
 import React from "react";
 import MainHeader from "../../components/header/MainHeader";
-import Header from "../../components/header/Header";
 
 function Home() {
   return (
     <div>
       <MainHeader title="홈" />
-
-      <Header title="식물등록" />
-
-      <main style={{ padding: "20px" }}>
-        <p>헤더 확인용 화면입니다.</p>
-      </main>
     </div>
   );
 }
